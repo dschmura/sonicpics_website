683e06b49efa34b966b8c31c4668ae15e70f344c
Dave Chmura <dschmura@humbledaisy.com>
2015-03-02 07:03:08 -0500

    added assets (images etc...)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
@@ -14,3 +14,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//= require bootstrap
+
+
+
+/*menu handler. Sets current link to active in nav bars in header and footer for current page.*/
+$(function(){
+  function stripTrailingSlash(str) {
+    
+    if(str.substr(-1) == '/') {
+      return str.substr(0, str.length - 1);
+    }
+    return str;
+  }
+
+  var url = window.location.pathname;  
+  var activePage = stripTrailingSlash(url);
+
+  $('.nav li a').each(function(){  
+    var currentPage = stripTrailingSlash($(this).attr('href'));
+
+    if (activePage == currentPage){
+      $(this).parent().addClass('active'); 
+    }
+   
+  });
+});
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def page_title
+    base_title = "SonicPics"
+    @base_title = base_title
+    if @page_title.nil?
+      base_title
+    else
+      "#{@page_title} | #{base_title}"
+    end
+  end
+
+
+  def footer_logo
+   image_tag("LSA_ISS_Pill.png", :alt => "#{@base_title} Humble Daisy, Inc",
+                                 :style => "height: 44px; float: left; position: relative; right: 22px; margin: 0px;")
+  end
+
+  def logo
+    image_tag("SonicPicsV2.png", :alt => "SonicPics Logo", :id => "logo")
+  end
+
+
 end
