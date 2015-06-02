4b6f0e84f7217f32ff74f0570c9f347a7a60bdeb
Dave Chmura <dschmura@humbledaisy.com>
2015-06-02 09:39:37 -0400

    updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
 set :application, 'sonicpics_website'
 set :repo_url, 'git@github.com:dschmura/sonicpics_website.git'
-set :linked_files, %w{config/database.yml}
 set :deploy_to, "/home/deployer/apps/#{fetch(:application)}"
