ea63ba0473b54a20fa6107aabc837c4b75683e57
Dave Chmura <dschmura@humbledaisy.com>
2015-07-01 16:59:22 -0400

    updated cap files to point to new directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
 set :application, 'sonicpics_website'
 set :use_sudo, false
 set :repo_url, 'git@github.com:dschmura/sonicpics_website.git'
-set :deploy_to, "/home/deployer/apps/#{fetch(:application)}"
+set :deploy_to, "/home/deployer/apps/sonicpics_website"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server "198.199.92.58", user: 'deployer', roles: %w{app db web}
 set :stage, :production
 set :deploy_user, "deployer"
-
+set :full_app_name, "sonicpics_website"
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
 set :branch, 'master'
