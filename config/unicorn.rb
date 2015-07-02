151190e07f7034e662403086ece856e82daed094
Dave Chmura <dschmura@humbledaisy.com>
2015-07-02 08:57:28 -0400

    nothing to say here

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 root = "/home/deployer/apps/sonicpics_website/current"
 working_directory root
-pid "#{root}/tmp/pids/sonicpics_website.pid"
+pid "#{root}/tmp/pids/sonicpics_website_production.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
