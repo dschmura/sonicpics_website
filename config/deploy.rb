2940772ea7f38b071165d4498a40e96e05cb4bca
David Chmura <dschmura@humbledaisy.com>
2016-02-11 15:24:30 -0500

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,4 @@ set :application, 'sonicpics_website'
 set :use_sudo, false
 set :repo_url, 'git@github.com:dschmura/sonicpics_website.git'
 set :deploy_to, "/home/deployer/apps/sonicpics_website"
+set :pty, true
